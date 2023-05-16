e3990779d20123c9a6ab4bb7e2fb4b8cc64aa0af
suongfiori <suong.kf@gmail.com>
2023-05-16 14:15:30 +0100

    Added a new solution

--- 8kyu/categorize-new-member.js
+++ 8kyu/categorize-new-member.js
@@ -22,3 +22,7 @@ function openOrSenior(data){
       }
       return result;
     }
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+    }
